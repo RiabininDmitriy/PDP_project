83e25495523bcd0379234181ff667448407465e5
DmytroRiabinin <dmitriy.riabinin@tech-stack.io>
2025-03-06 12:59:33 +0200

    add battle-log

--- Back/src/modules/battleLog/battleLog.module.ts
+++ Back/src/modules/battleLog/battleLog.module.ts
@@ -2,7 +2,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BattleLog } from 'src/entities/battle-log.entity';
-import { BattleLogService } from '../battleLog/battleLog.service';
+import { BattleLogService } from './battleLog.service';
 import { BattleLogRepository } from './battleLog.repo';
 import { BattleLogController } from './battleLog.controller';
 import { UserModule } from '../user/user.module';
